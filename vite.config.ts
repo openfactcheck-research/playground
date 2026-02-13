3ed5907813692ce4172f49b7d44d48056a5c4947
Hasan Iqbal <34169605+hasaniqbal777@users.noreply.github.com>
2026-02-13 09:14:07 +0400

    Added tailwindcss and shadcn/vue

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
+import path from 'node:path'
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   define: {
